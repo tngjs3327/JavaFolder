09c3b3096329b0cbf5858382c3acb371c38475d4
tngjs3327 <68262812+tngjs3327@users.noreply.github.com>
2023-03-03 16:49:23 +0900

    Add files via upload

---
+++ JavaFolder/Weeks1_2/Television.java
@@ -0,0 +1,12 @@
+package Weeks1_2;
+
+public class Television {
+  int channel; // 채널 번호
+  int volume; // 볼륨
+  boolean isOn; // 전원상태
+
+  void print() {
+    System.out.println("채널은 " + channel + "이고 볼륨은 "
+        + volume + "입니다.");
+  }
+}
