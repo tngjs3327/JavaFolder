51fbee64ef5ccf4b178ed1ba04c0ee1e4dc7fcda
tngjs3327 <68262812+tngjs3327@users.noreply.github.com>
2023-05-15 20:49:48 +0900

    Add files via upload

---
+++ Weeks12_1/MyInterface1.java
@@ -0,0 +1,5 @@
+package Weeks12_1;
+
+public interface MyInterface1 {
+  void print(String s);
+}
---
+++ Weeks12_1/Test.java
@@ -0,0 +1,27 @@
+package Weeks12_1;
+
+public class Test {
+  public void test(MyInterface1 m1) {
+    m1.print("Hello World");
+  }
+
+  public static void main(String[] args) {
+    Test t = new Test();
+
+    // 1. 무명 클래스를 이용해 Test 클래스의 인스턴스 메소드 test 호출하시오
+    // t.test(new MyInterface1() {
+    // @Override
+    // public void print(String s) {
+    // System.out.println(s);
+    // }
+    // });
+
+    // 2. 람다를 이용해 Test 클래스의 인스턴스 메소드 test 호출하세요
+    // 인터페이스인데, 오직 하나의 추상 메소드를 가지는 인터페이스는
+    // 람다로 구현할 수 있다
+    t.test((s) -> {
+      System.out.println(s);
+    });
+
+  }
+}
